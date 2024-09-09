409d379c3dcc33a576414ecd32c823d50b54b431
kat <130811683+xxwhirlpool@users.noreply.github.com>
2024-09-09 14:55:32 -0400

    Update weather.sh

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -13,6 +13,6 @@ WEATHER=$(curl https://wttr.in/New+York?format=3)
 TITLE="weather"
 MESSAGE="${WEATHER}"
 PRIORITY=5
-URL="https://gotify.girlonthemoon.xyz/message?token=Apqq2MOk_yLflvH"
+URL="https://url/message?token=slay"
 
 curl -s -S --data '{"message": "'"${MESSAGE}"'", "title": "'"${TITLE}"'", "priority":'"${PRIORITY}"', "extras": {"client::display": {"contentType": "text/markdown"}}}' -H 'Content-Type: application/json' "$URL"

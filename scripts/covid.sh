842c9c4973009233d0c4c5d93105ce3f795f210c
kat <130811683+xxwhirlpool@users.noreply.github.com>
2024-09-09 17:22:39 -0400

    Update covid.sh

--- scripts/covid.sh
+++ scripts/covid.sh
@@ -4,6 +4,9 @@
 # https://open-source-software-ph.gitbook.io/covid19-tracker-cli/getting-started/basic-usage#country-tracking
 #
 # replace https://url/message?token=slay as needed - URL with your reverse proxied gotify site (https://gotify.net/docs/caddy), token with your app token after you've created it in the web UI for apps (https://gotify.net/docs/pushmsg)
+#
+# crontab schedule i use for this:
+# 0 10,15 *  * * /path/to/covid.sh
 
 COVID="$(curl -L https://covid19-cli.wareneutron.com/quiet/basic/usa)"
 CMD="$(echo -n ${COVID})"
